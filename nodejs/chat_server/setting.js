6eabbcb8b8268dc4e18ba969eac62dc404a7aa5f
kimura-tsuyoshi <maxim_4141.rock@zoho.com>
2017-11-19 23:28:15 +0900

    [add] チャットサーバとMongoDBを連携

--- nodejs/chat_server/setting.js
+++ nodejs/chat_server/setting.js
@@ -1,3 +1,6 @@
-exports.port = 1337;
-exports.host = '192.168.33.70';
-exports.db   = 'nodedb';
+exports.port       = 1337;
+exports.host       = '192.168.33.70';
+exports.db         = 'nodedb';
+exports.db_host    = 'localhost';
+exports.collection = 'chat';
+exports.document   = 'chat_data';

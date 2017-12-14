27c93c4e0eba88344eaa37c8c4008603c3aed239
kimura-tsuyoshi <maxim_4141.rock@zoho.com>
2017-12-15 02:28:36 +0900

    [add] Sales.jsで「Hello, World!」を出力。

---
+++ nodejs/Sails/sails-tutorial/api/controllers/ApplicationController.js
@@ -0,0 +1,18 @@
+/**
+ * ApplicationController
+ *
+ * @description :: Server-side logic for managing applications
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+
+
+  /**
+   * `ApplicationController.hello()`
+   */
+  hello: function (req, res) {
+    return res.send('Hello, World!');
+  }
+};

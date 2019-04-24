231fb1ed6a3500c7904077d9b94596b8211bb82c
juve534 <maxim_4141.rock@zoho.com>
2019-04-25 02:24:30 +0900

    :sparkles: ドキュメントの削除
    Go言語でドキュメントを削除するコードの写経.

---
+++ Elasticsearch/code/delete.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/olivere/elastic"
+)
+
+func main() {
+	esURL := "http://127.0.0.1:9200"
+	ctx := context.Background()
+	client, err := elastic.NewClient(
+		elastic.SetURL(esURL),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	deleted, err := client.Delete().Index("chat").Type("chat").Id("I").Do(ctx)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf(deleted.Result)
+}

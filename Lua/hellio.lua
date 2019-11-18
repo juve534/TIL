ae4160a5b4f7f89ba4b7365b9c58f738af511317
juve534 <maxim_4141.rock@zoho.com>
2019-11-19 01:17:40 +0900

    [lesson] Luaの学習 (#119)
    * :books: 数値の扱いを学習
    
    * :books: 文字列について学習
    
    https://dotinstall.com/lessons/basic_lua/11005
    
    * :books: テーブルについて学習
    
    https://dotinstall.com/lessons/basic_lua/11006
    
    * :books: if文について学習
    
    https://dotinstall.com/lessons/basic_lua/11007
    
    * :books: 関係演算子と論理演算子を学習
    
    https://dotinstall.com/lessons/basic_lua/11008

--- Lua/hellio.lua
+++ Lua/hellio.lua
@@ -5,3 +5,41 @@ print(message)
 -- 多重代入
 x, y = 10, 15
 print(x, y)
+
+-- 数値
+x = 10 + 10
+print(x)
+
+x = 10.5 * 4.2
+print(x)
+
+x = 10 % 3
+print(x)
+
+x = 2 ^ 3
+print(x)
+
+-- 文字列
+s = "123\n456"
+print(s)
+
+print([["123\n456"]])
+
+-- テーブル(配列/連想配列)
+a = {23, 234, "hello"}
+print(a[2])
+print(#a)
+
+user = {name = "test", score = 120}
+print(user["name"])
+print(user.score)
+
+-- 条件分岐
+score = 45
+if score > 60 then
+    print("OK!")
+elseif score > 40 then
+    print("SELF")
+else
+    print("NG!")
+end

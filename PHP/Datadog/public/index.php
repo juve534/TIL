bf2cb6159887e97d8ececa1c84a61337edb4e1be
juve534 <maxim_4141.rock@zoho.com>
2020-09-03 02:36:51 +0900

    :sparkles: Datadogのイベントを登録するエンドポイントを追加

--- PHP/Datadog/public/index.php
+++ PHP/Datadog/public/index.php
@@ -2,17 +2,25 @@
 
 declare(strict_types=1);
 
+require __DIR__ . '/../vendor/autoload.php';
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-
-require __DIR__ . '/../vendor/autoload.php';
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 $app = AppFactory::create();
 
 $app->get('/hello', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello world!");
     return $response;
+});
+
+$app->group('/datadog', function (Group $group) {
+    $group->post('/event', function (Request $request, Response $response, $args) {
+        $event = new \Datadog\Event(new \GuzzleHttp\Client(), getenv('DD_API_KEY'));
+        return $response->withStatus(204);
+    });
 });
 
 $app->run();

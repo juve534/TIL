042c88f87ce234aa6c7ab1790717799d54d4110f
juve534 <maxim_4141.rock@zoho.com>
2022-01-14 17:01:08 +0900

    :sparkles: VPCを作成する
    VPCを作成するコードを記載.
    勉強用なのでAZは1つで作る.

--- AWS_CDK/hello/lib/hello-stack.ts
+++ AWS_CDK/hello/lib/hello-stack.ts
@@ -1,16 +1,16 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Vpc } from "aws-cdk-lib/aws-ec2";
 
 export class HelloStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new Vpc(this, 'VPC', {
+      maxAzs: 1,
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CfnOutput(this, 'Vpc', { value: vpc.vpcId })
+    new CfnOutput(this, 'DefaultSG', { value: vpc.vpcDefaultSecurityGroup })
   }
 }
